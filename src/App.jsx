22e66a6969ef803076d9daec323fdc54921565e1
Ysedda <arthur.baudoin@icloud.com>
2022-10-05 11:45:08 +0200

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div>
+      <Routes>
+        <Route path="/" />
+        <Route path="/play" />
+        <Route path="/learn" />
+        <Route path="/about" />
+      </Routes>
       <Navbar />
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="bg-greyblue text-white shadow-lg hidden md:block">
       <div className="flex justify-around h-24 w-full px-4">
-        <a href="" className="flex items-center justify-center">
+        <Link to="/" className="flex items-center justify-center">
           <img className="h-16" src="../../icon.png" alt="" />
           <span className="ml-4 font-semibold text-3xl">Map Quest</span>
-        </a>
+        </Link>
         <nav className="contents font-semibold justify-between text-base lg:text-lg">
           <ul className="mx-auto flex items-center">
             <li className="p-5 xl:p-8">
-              <a href="">
+              <Link to="/play">
                 <span className="text-2xl">Play</span>
-              </a>
+              </Link>
             </li>
             <li className="p-5 xl:p-8">
-              <a href="">
+              <Link to="/learn">
                 <span className="text-2xl">Learn</span>
-              </a>
+              </Link>
             </li>
           </ul>
           <div className="flex items-center">
-            <a className="p-5 xl:p-8">About</a>
+            <Link to="/about" className="p-5 xl:p-8">
+              About
+            </Link>
           </div>
         </nav>
       </div>
