f825ebb093c0476fa48aeef1520638da3ef9f938
ngroult <nolwenn.groult@gmail.com>
2022-10-05 17:05:08 +0200

    Add Page How To Play

---
+++ src/components/howToPlay/HowToPlay.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+const HowToPlay = () => {
+  return (
+    <div className="border border-black rounded-lg p-10">
+      <h1 className="text-3xl font-bold my-10">How To Play ?</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <fieldset className=" my-10">
+        <legend>Choose your game mode :</legend>
+        <div className="flex justify-between my-4">
+          <div className="">
+            <input
+              type="checkbox"
+              id="capital"
+              name="capital"
+              className="rounded-full mx-2"
+            />
+            <label htmFor="capital">Capitals</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="currency"
+              name="currency"
+              className="rounded-full mx-2"
+            />
+            <label>Currencies</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="language"
+              name="language"
+              className="rounded-full mx-2"
+            />
+            <label>Languages</label>
+          </div>
+        </div>
+      </fieldset>
+
+      <div className="flex justify-center">
+        <button className="border border-black bg-lightgreen rounded-lg px-10 py-4 my-4">
+          Play
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default HowToPlay;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightgreen: "#C7F6C3",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
