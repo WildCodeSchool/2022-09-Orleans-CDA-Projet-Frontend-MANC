6dc7376419c23b4780b3d8efbee221e2137b3a71
codelande <100156552+codelande@users.noreply.github.com>
2022-10-07 15:33:38 +0200

    addd questio non play (#13)

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,9 +1,11 @@
 import Map from "../components/map/Map";
+import Question from "../components/question/Question";
 
 const Quiz = () => {
   return (
     <div className="height-minus-nav">
       <Map />
+      <Question />
     </div>
   );
 };
