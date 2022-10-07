9a6bc59650d785326ba2de9df49876663f0dd8dd
Gliem <maximilian.gliem@gmail.com>
2022-10-07 09:50:18 +0200

    add routes and home pages on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import { MdBook, MdNotStarted } from "react-icons/md";
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div>
-      <div className="absolute z-10 flex flex-col items-center w-full">
+      <div className="absolute z-10 flex flex-col items-center height-minus-nav">
         <h1 className="text-8xl mt-20 w-fit">Map Quest</h1>
         <p className="mx-40 mt-20 text-2xl text-center">
           Lorem ipsum dolor sit amet. Qui velit eligendi et explicabo
@@ -15,15 +18,19 @@ function Home() {
           vel itaque sunt atque omnis.
         </p>
         <div className="flex justify-evenly mt-48 text-2xl w-full">
-          <button className="drop-shadow-2xl border-double border-4 p-4 rounded-md border-black bg-teal-500">
-            [icon] bouton
-          </button>
-          <button className="drop-shadow-2xl border-double border-4 p-4 rounded-md border-black bg-red-500">
-            [icon] bouton
-          </button>
+          <Link to="/play">
+            <button className="flex items-center gap-2 drop-shadow-2xl border-double border-4 p-4 rounded-md border-black bg-teal-500">
+              Play <MdNotStarted className="text-2xl" />
+            </button>
+          </Link>
+          <Link to="/learn">
+            <button className="flex items-center gap-2 drop-shadow-2xl border-double border-4 p-4 rounded-md border-black bg-red-500">
+              <MdBook className="text-2xl" /> Learn
+            </button>
+          </Link>
         </div>
       </div>
-      <div className="bg-[url('./src/assets/images/blurred-map.jpg')] contrast-75 bg-no-repeat bg-cover blur-[2.5px] grayscale h-screen w-screen"></div>
+      <div className="bg-[url('./assets/images/blurred-map.jpg')] contrast-75 bg-no-repeat bg-cover blur-[2.5px] grayscale height-minus-nav w-screen"></div>
     </div>
   );
 }
