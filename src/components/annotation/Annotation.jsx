cd1c742ea77069846f87d069940dfd95cee5a978
Ysedda <arthur.baudoin@icloud.com>
2022-10-07 14:47:54 +0200

    country details component

--- src/components/annotation/Annotation.jsx
+++ src/components/annotation/Annotation.jsx
@@ -5,37 +5,41 @@ import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
 import { useEffect, useState } from "react";
 
 const Annotation = () => {
-  const [countryName, setCountryName] = useState("");
-  const [countryCurrency, setCountryCurrency] = useState("");
-  const [countryLanguage, setCountryLanguage] = useState({});
+  const [countryData, setCountryData] = useState();
 
   async function getResponse() {
-    const res = await fetch("https://restcountries.com/v3.1/name/peru");
+    const res = await fetch("https://restcountries.com/v3.1/name/uae");
     const data = await res.json();
-    setCountryName(data[0].name.common);
-    // setCountryCurrency(data[0].languages);
-    // setCountryLanguage(data[0].languages);
-    // console.log(data[0].languages);
-    // console.log(countryLanguage);
+    setCountryData(data[0]);
   }
 
   useEffect(() => {
     getResponse();
-  });
+  }, []);
 
   return (
     <>
-      <div className="flex flex-col w-96 justify-center p-6 shadow-lg gap-2 rounded-md border border-greyblue border-solid m-5">
-        <h2 className="text-2xl text-center font-semibold mb-4">France</h2>
+      <div className="flex w-fit flex-col justify-center p-6 shadow-lg gap-2 rounded-md border border-greyblue border-solid m-5">
+        <h2 className="text-2xl text-center font-semibold mb-4">
+          {countryData && countryData.name.common}
+        </h2>
         <p className="flex items-center gap-2 text-lg">
           <TbBuilding />
-          {countryName}
+          Capital: {countryData && countryData.capital[0]}
         </p>
         <p className="flex items-center gap-2 text-lg">
           <HiOutlineCurrencyDollar />
           Currency:{" "}
+          {countryData && Object.values(countryData.currencies)[0].name}
         </p>
-        {/* <p className="flex items-center gap-2 text-lg"><IoChatbubbleEllipsesOutline />Language: {countryLanguage.map(language => <span>{language}</span>)}</p> */}
+        <p className="flex items-center gap-2 text-lg">
+          <IoChatbubbleEllipsesOutline />
+          Languages:{" "}
+          {countryData &&
+            Object.values(countryData.languages).map((language, index) => (
+              <span key={index}>{language}</span>
+            ))}
+        </p>
       </div>
     </>
   );
