e79f774d9f0f80171a5a9e6e70976e23920b235f
Ysedda <86974668+Ysedda@users.noreply.github.com>
2022-10-12 15:33:06 +0200

    add country details to search result and color change on click (#19)

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -6,8 +6,15 @@ import {
   Marker,
 } from "react-simple-maps";
 
-const Map = ({ actionOnClick, searchCountry, markerCoordinates }) => {
+const Map = ({
+  actionOnClick,
+  searchCountry,
+  setCountryFound,
+  clickedCountry,
+  markerCoordinates,
+}) => {
   const searchLower = searchCountry ? searchCountry.toLowerCase() : "";
+
   return (
     <>
       <ComposableMap
@@ -25,10 +32,16 @@ const Map = ({ actionOnClick, searchCountry, markerCoordinates }) => {
               geographies.map((geo) => (
                 <Geography
                   className={`${
-                    geo.properties.name.toLowerCase() === searchLower
+                    geo.properties.name.toLowerCase() === searchLower &&
+                    "fill-green-500"
+                  } ${
+                    geo.id.includes(clickedCountry) && clickedCountry !== ""
                       ? "fill-green-500"
                       : "fill-white"
-                  } stroke-0.5 stroke-slate-500  hover:fill-slate-700 hover:stroke-0`}
+                  } 
+                  stroke-0.5 stroke-slate-500  hover:fill-slate-700 hover:stroke-0`}
+                  {...(geo.properties.name.toLowerCase() === searchLower &&
+                    setCountryFound(geo.id))}
                   key={geo.rsmKey}
                   geography={geo}
                   onClick={() => {
