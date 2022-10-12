143a30759a15abc86616c2474b27e9f7a29bfd4d
Ysedda <arthur.baudoin@icloud.com>
2022-10-12 15:26:22 +0200

    added aos and fade-in effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 function Home() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div>
       <div className="absolute z-10 flex flex-col items-center height-minus-nav ">
-        <h1 className="text-8xl w-fit bg-white bg-opacity-80 rounded-xl p-10 m-20 border-solid border-2 border-greyblue shadow-2xl">
+        <h1
+          data-aos="fade-down"
+          data-aos-duration="600"
+          className="text-8xl w-fit bg-white bg-opacity-80 rounded-xl p-10 m-20 border-solid border-2 border-greyblue shadow-2xl"
+        >
           <span className="text-green-400">Map</span>
           <span className="text-greyblue">Quest</span>{" "}
         </h1>
-        <p className="mx-40 text-2xl bg-white bg-opacity-80 rounded-xl p-10 text-center border-solid border-2 border-greyblue">
+        <p
+          data-aos="fade-up"
+          data-aos-duration="600"
+          className="mx-40 text-2xl bg-white bg-opacity-80 rounded-xl p-10 text-center border-solid border-2 border-greyblue"
+        >
           Lorem ipsum dolor sit amet. Qui velit eligendi et explicabo
           consequatur ea saepe tempore et voluptatum? Quo voluptatum debitis eos
           repellat ipsa ut autem neque vel itaque sunt atque omnis. Lorem ipsum
