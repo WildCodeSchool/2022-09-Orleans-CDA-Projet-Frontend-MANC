8fafec65fca725f74e2aa16f71478e7aceca53d7
Ysedda <86974668+Ysedda@users.noreply.github.com>
2022-10-19 14:53:17 +0200

    Feature/ answer question all types (#33)
    * array of question types
    
    * almost working
    
    * ca marche ?
    
    * it works!
    
    * remove logs
    
    * aj
    
    * typo
    
    * double rendering still and errors in console
    
    * added condition to trigger fetch
    
    * refactoring question type logic, fix double question generation
    
    * const instead of let, variable declaration spot and condition refactorisation
    
    Co-authored-by: codelande <corentin.delande.1@gmail.com>

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -8,12 +8,59 @@ import countryData from "../assets/countriesData.json";
 const Quiz = () => {
   const location = useLocation();
   const gameModes = location.state;
+  const [questionType, setQuestionType] = useState(null);
+  const [countryAnswer, setCountryAnswer] = useState(null);
+
+  const getAnswer = (clickedCountry) => {
+    if (clickedCountry !== "") {
+      fetch(`https://restcountries.com/v3.1/alpha/${clickedCountry}`)
+        .then((resp) => resp.json())
+        .then((data) => {
+          if (questionType.type === "currencies") {
+            setCountryAnswer(data[0].currencies);
+          }
+          if (questionType.type === "languages") {
+            setCountryAnswer(data[0].languages);
+          }
+        });
+    }
+  };
+
   const [clickedCountry, setClickedCountry] = useState("");
+  const [preventClickCountry, setPreventClickCountry] = useState(false);
+
+  useEffect(() => {
+    getAnswer(clickedCountry);
+    const clickedCountryData = countryData.find((data) => {
+      return data.id === clickedCountry;
+    });
+    if (clickedCountryData) {
+      setMarkerCoordinates(clickedCountryData.coord);
+    }
+  }, [clickedCountry]);
+
   const [question, setQuestion] = useState("");
+
+  useEffect(() => {
+    setAnswer({ isAnswered: false, isCorrect: false });
+  }, [question]);
+
+  useEffect(() => {
+    setIsConfirmed(false);
+  }, [question]);
+
   const [answer, setAnswer] = useState({ isAnswered: false, isCorrect: false });
+
+  useEffect(() => {
+    setPreventClickCountry(false);
+  }, [question]);
+
+  useEffect(() => {
+    !answer.isAnswered && setClickedCountry("");
+  }, [answer]);
+
   const [markerCoordinates, setMarkerCoordinates] = useState("");
   const [isConfirmed, setIsConfirmed] = useState(false);
-  const [preventClickCountry, setPreventClickCountry] = useState(false);
   const [counterCorrect, setCounterCorrect] = useState(0);
 
   useEffect(() => {
@@ -25,22 +72,37 @@ const Quiz = () => {
   }, [isConfirmed]);
 
   useEffect(() => {
-    const clickedCountryData = countryData.find((data) => {
-      return data.id === clickedCountry;
-    });
-    if (clickedCountryData) {
-      setMarkerCoordinates(clickedCountryData.coord);
+    if (isConfirmed) {
+      if (number === 0) {
+        setAnswer({
+          isAnswered: true,
+          isCorrect: clickedCountry === question[0].cca3,
+        });
+      }
+      if (number === 1) {
+        setAnswer({
+          isAnswered: true,
+          isCorrect: Object.values(Object.values(countryAnswer)[0]).includes(
+            Object.values(Object.values(question[0].currencies)[0])[0]
+          ),
+        });
+      }
+      if (number === 2) {
+        setAnswer({
+          isAnswered: true,
+          isCorrect: Object.values(countryAnswer).includes(
+            Object.values(question[0].languages)[0]
+          ),
+        });
+      }
     }
-  }, [clickedCountry]);
+  }, [isConfirmed]);
 
   useEffect(() => {
     isConfirmed && setMarkerCoordinates("");
   }, [isConfirmed]);
 
-  useEffect(() => {
-    setAnswer({ isAnswered: false, isCorrect: false });
-  }, [question]);
-
+  const [number, setNumber] = useState(null);
   useEffect(() => {
     setIsConfirmed(false);
   }, [question]);
@@ -71,7 +133,11 @@ const Quiz = () => {
         question={question}
         setQuestion={setQuestion}
         isAnswered={answer.isAnswered}
+        questionType={questionType}
         gameModes={gameModes}
+        number={number}
+        setQuestionType={setQuestionType}
+        setNumber={setNumber}
         setPreventClickCountry={setPreventClickCountry}
       />
       {answer.isAnswered && clickedCountry !== "" && (
