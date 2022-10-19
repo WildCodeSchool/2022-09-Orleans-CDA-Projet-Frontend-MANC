e9fc366bd20337c317f13162f622bfb4ab418827
Ysedda <arthur.baudoin@icloud.com>
2022-10-19 17:10:13 +0200

    added aos and changed component spot

--- src/components/answer/Answer.jsx
+++ src/components/answer/Answer.jsx
@@ -1,11 +1,21 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
 const Answer = ({ answer, questionType, response }) => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <div
       className={`${
         answer ? "bg-green-500" : "bg-red-500"
-      } top-1/2 left-1/2 absolute flex flex-col border-1 shadow-xl border-top border-solid border-black p-6 rounded-xl`}
+      } answer left-0 right-0 mx-auto w-fit mt-4 absolute flex flex-col border-1 shadow-xl border-top border-solid border-black p-6 rounded-xl`}
+      data-aos="zoom-in"
+      data-aos-duration="400"
     >
-      <h1 className="text-white text-2xl text-center mb-6 font-semibold">
+      <h1 className="text-white text-2xl text-center mb-4 font-semibold">
         {answer ? "Good!" : "Wrong!"}
       </h1>
       {questionType.type === "capital" ? (
