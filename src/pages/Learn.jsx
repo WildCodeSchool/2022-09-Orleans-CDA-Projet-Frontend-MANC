e1f3ee45fcf3150e18bbebb4b2c5fad846b52c6d
Gliem <maximilian.gliem@gmail.com>
2022-10-19 11:53:19 +0200

    marker coordinates on found

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -2,15 +2,29 @@ import { useEffect, useState } from "react";
 import Annotation from "../components/annotation/Annotation";
 import Map from "../components/map/Map";
 import SearchLearn from "../components/searchLearn/SearchLearn";
+import countries from "../assets/countriesData.json";
 
 const Learn = () => {
   const [clickedCountry, setClickedCountry] = useState("");
   const [countrySearchFound, setCountrySearchFound] = useState("");
   const [countryFound, setCountryFound] = useState("");
+  const [markerFoundCoordinate, setMarkerFoundCoordinate] = useState("");
 
   useEffect(() => {
     setClickedCountry(countryFound);
   }, [countryFound]);
+
+  useEffect(() => {
+    if (countrySearchFound) {
+      console.log(countrySearchFound);
+      const foundCountryData = countries.find((data) => {
+        return data.id === countrySearchFound.cca3;
+      });
+      if (foundCountryData) {
+        setMarkerFoundCoordinate(foundCountryData.coord);
+      }
+    }
+  }, [countrySearchFound]);
 
   return (
     <div className="height-minus-nav flex">
@@ -21,6 +35,7 @@ const Learn = () => {
           searchCountry={countrySearchFound.name.common}
           setCountryFound={setCountryFound}
           clickedCountry={clickedCountry}
+          markerFoundCoordinate={markerFoundCoordinate}
         />
       ) : (
         <Map actionOnClick={setClickedCountry} />
