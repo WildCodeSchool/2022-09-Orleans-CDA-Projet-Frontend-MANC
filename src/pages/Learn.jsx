312118956ca7b379f1dc90ab6b0435dabc2d5a3b
Gliem <maximilian.gliem@gmail.com>
2022-10-19 17:14:01 +0200

    change on learn

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -13,20 +13,22 @@ const Learn = () => {
   }, [countryFound]);
 
   return (
-    <div className="height-minus-nav flex">
-      <SearchLearn setCountrySearchFound={setCountrySearchFound} />
-      {countrySearchFound ? (
-        <Map
-          actionOnClick={setClickedCountry}
-          searchCountry={countrySearchFound.name.common}
-          setCountryFound={setCountryFound}
-          clickedCountry={clickedCountry}
-        />
-      ) : (
-        <Map actionOnClick={setClickedCountry} />
-      )}
+    <div className="height-minus-nav flex justify-center items-center bg-quiz">
+      <div className="h-4/5 w-4/5 p-1 relative flex rounded-lg border-solid border-2 border-cyan-900 shadow-2xl bg-cyan-900">
+        <SearchLearn setCountrySearchFound={setCountrySearchFound} />
+        {countrySearchFound ? (
+          <Map
+            actionOnClick={setClickedCountry}
+            searchCountry={countrySearchFound.name.common}
+            setCountryFound={setCountryFound}
+            clickedCountry={clickedCountry}
+          />
+        ) : (
+          <Map actionOnClick={setClickedCountry} />
+        )}
 
-      {clickedCountry && <Annotation country={clickedCountry} />}
+        {clickedCountry && <Annotation country={clickedCountry} />}
+      </div>
     </div>
   );
 };
