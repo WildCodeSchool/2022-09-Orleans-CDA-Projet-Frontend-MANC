7afa0a686f20f092677575d8573b076c47171de1
MGliem <100389157+MGliem@users.noreply.github.com>
2022-10-19 15:35:18 +0200

    Feature/marker on search (#34)
    * marker coordinates on found
    
    * marker on map
    
    * set marker coordinate to empty when no country found
    
    * removed console log in learn

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 import Annotation from "../components/annotation/Annotation";
 import Map from "../components/map/Map";
 import SearchLearn from "../components/searchLearn/SearchLearn";
+import countries from "../assets/countriesData.json";
 
 const Learn = () => {
   const [clickedCountry, setClickedCountry] = useState("");
   const [countrySearchFound, setCountrySearchFound] = useState("");
+  const [markerFoundCoordinate, setMarkerFoundCoordinate] = useState("");
 
   useEffect(() => {
     setClickedCountry(countrySearchFound);
@@ -15,12 +17,26 @@ const Learn = () => {
     setCountrySearchFound(clickedCountry);
   }, [clickedCountry]);
 
+  useEffect(() => {
+    if (countrySearchFound) {
+      const foundCountryData = countries.find((data) => {
+        return data.id === countrySearchFound.cca3;
+      });
+      if (foundCountryData) {
+        setMarkerFoundCoordinate(foundCountryData.coord);
+      }
+    } else {
+      setMarkerFoundCoordinate("");
+    }
+  }, [countrySearchFound]);
+
   return (
     <div className="height-minus-nav flex">
       <SearchLearn setCountrySearchFound={setCountrySearchFound} />
       <Map
         actionOnClick={setClickedCountry}
         clickedCountry={clickedCountry ? clickedCountry : countrySearchFound}
+        markerFoundCoordinate={markerFoundCoordinate}
       />
       {clickedCountry && <Annotation country={clickedCountry} />}
     </div>
