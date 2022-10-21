517070aa81f8f5e34bb1abd21343a7f7646909ec
Ysedda <86974668+Ysedda@users.noreply.github.com>
2022-10-21 11:56:22 +0200

    padding and spacing in search bar and game details (#58)

--- src/components/recapGame/RecapGame.jsx
+++ src/components/recapGame/RecapGame.jsx
@@ -5,9 +5,9 @@ const RecapGame = ({
   response,
 }) => {
   return (
-    <div className="z-50 absolute h-3/4 right-0 m-10">
+    <div className="z-50 absolute top-32 right-0 m-10">
       <div className="my-10 border-2 w-auto border-solid shadow-2xl p-4 rounded-2xl border-black bg-slate-100">
-        <h2 className="text-center w-full text-3xl">Details</h2>
+        <h2 className="text-center w-full text-3xl mb-4">Details</h2>
         <p>{`Question : ${counterQuestion}/${questionNumber}`}</p>
         <p>Score : {counterCorrect}</p>
         <p>
