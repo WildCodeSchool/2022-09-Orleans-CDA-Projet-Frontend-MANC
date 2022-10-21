96f46003a780abc4a0fef6a733b6896144876b0a
MGliem <100389157+MGliem@users.noreply.github.com>
2022-10-21 09:50:41 +0200

    map width 90% (#55)

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -131,7 +131,7 @@ const Quiz = () => {
         setNumber={setNumber}
         setPreventClickCountry={setPreventClickCountry}
       />
-      <div className="h-4/5 w-4/5 p-1 relative flex rounded-lg border-solid border-2 border-cyan-900 shadow-2xl bg-cyan-900">
+      <div className="h-4/5 w-[90%] p-1 relative flex rounded-lg border-solid border-2 border-cyan-900 shadow-2xl bg-cyan-900">
         <Map
           preventClickCountry={preventClickCountry}
           clickedCountry={clickedCountry}
