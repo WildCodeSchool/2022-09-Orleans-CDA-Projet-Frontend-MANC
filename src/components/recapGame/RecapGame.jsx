a6be264817508efda0a2e7db45dafdfe9001eed7
ngroult <nolwenn.groult@gmail.com>
2022-10-21 09:45:10 +0200

    correction yarn format

--- src/components/recapGame/RecapGame.jsx
+++ src/components/recapGame/RecapGame.jsx
@@ -8,9 +8,7 @@ const RecapGame = ({
     <div className="z-50 absolute h-3/4 right-0 m-10">
       <div className="my-10 border-2 w-auto border-solid shadow-2xl p-4 rounded-2xl border-black bg-slate-100">
         <h2 className="text-center w-full text-3xl">Details</h2>
-        <p>
-          Question : {counterQuestion}/{questionNumber}{" "}
-        </p>
+        <p>{`Question : ${counterQuestion}/${questionNumber}`}</p>
         <p>Score : {counterCorrect}</p>
         <p>
           {response !== "" && "Previous correct answer :"} {response}
