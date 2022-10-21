cfde7b00b8ac0157b5a05199f0505b2d4bb07305
Gliem <maximilian.gliem@gmail.com>
2022-10-21 09:40:25 +0200

    map width 90%

--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -32,7 +32,7 @@ const Learn = () => {
 
   return (
     <div className="height-minus-nav flex justify-center items-center bg-quiz">
-      <div className="h-4/5 w-4/5 p-1 relative flex rounded-lg border-solid border-2 border-cyan-900 shadow-2xl bg-cyan-900">
+      <div className="h-4/5 w-[90%] p-1 relative flex rounded-lg border-solid border-2 border-cyan-900 shadow-2xl bg-cyan-900">
         <SearchLearn setCountrySearchFound={setCountrySearchFound} />
         <Map
           actionOnClick={setClickedCountry}
