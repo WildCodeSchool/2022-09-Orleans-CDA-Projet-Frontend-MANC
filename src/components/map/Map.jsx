265f13e0366f24f84a539a73e6940ed739bbc0f0
Gliem <maximilian.gliem@gmail.com>
2022-10-25 16:58:56 +0200

    marker is no more growing when zooming in

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -21,7 +21,7 @@ const Map = ({
 
   useEffect(() => {
     if (markerCoordinates !== "") {
-      const defaultZoom = 3.5;
+      const defaultZoom = 2;
       setPosition((pos) => ({
         coordinates: markerCoordinates,
         zoom: pos.zoom < defaultZoom ? defaultZoom : pos.zoom,
@@ -94,7 +94,9 @@ const Map = ({
                 stroke="black"
                 strokeWidth="1"
                 strokeLinejoin="round"
-                transform="translate(-12, -24)"
+                transform={`translate(${-12 / position.zoom}, ${
+                  -24 / position.zoom
+                }) scale(${1 / position.zoom})`}
               >
                 <path d="M12 0c-4.198 0-8 3.403-8 7.602 0 6.243 6.377 6.903 8 16.398 1.623-9.495 8-10.155 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.342-3 3-3 3 1.343 3 3-1.343 3-3 3z" />
               </g>
