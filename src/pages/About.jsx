f32b4e35d04e7258214136899f7eaf935c115205
codelande <100156552+codelande@users.noreply.github.com>
2022-10-26 14:40:47 +0200

    add background video but no good size and firebase is bad (#70)
    * add background video but no good size and firebase is bad
    
    * video working again
    
    * resolve conflicts
    
    * resolve for real

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,40 +9,55 @@ function About() {
 
   return (
     <div>
-      <div className="absolute z-10 flex flex-col items-center height-minus-nav ">
-        <h1
-          data-aos="fade-down"
-          data-aos-duration="600"
-          className="text-8xl w-fit bg-white bg-opacity-80 rounded-xl p-8 m-20 border-solid border-2 border-greyblue shadow-2xl"
+      <header className="relative flex items-center justify-center h-screen overflow-hidden">
+        <div className="absolute z-30 flex flex-col items-center height-minus-nav ">
+          <h1
+            data-aos="fade-down"
+            data-aos-duration="600"
+            className="text-8xl w-fit m-24"
+          >
+            <span className="text-green-400">Map</span>
+            <span className="text-greyblue">Quest</span>
+          </h1>
+          <p
+            data-aos="fade-up"
+            data-aos-duration="600"
+            className="mx-40 text-2xl"
+          >
+            Lorem ipsum dolor sit amet. Qui velit eligendi et explicabo
+            consequatur ea saepe tempore et voluptatum? Quo voluptatum debitis
+            eos repellat ipsa ut autem neque vel itaque sunt atque omnis. Lorem
+            ipsum dolor sit amet. Qui velit eligendi et explicabo consequatur ea
+            saepe tempore et voluptatum? Quo voluptatum debitis eos repellat
+            ipsa ut autem neque vel itaque sunt atque omnis. Lorem ipsum dolor
+            sit amet. Qui velit eligendi et explicabo consequatur ea saepe
+            tempore et voluptatum? Quo voluptatum debitis eos repellat ipsa ut
+            autem neque vel itaque sunt atque omnis. Lorem ipsum dolor sit amet.
+            Qui velit eligendi et explicabo consequatur ea saepe tempore et
+            voluptatum? Quo voluptatum debitis eos repellat ipsa ut autem neque
+            vel itaque sunt atque omnis. Lorem ipsum dolor sit amet. Qui velit
+            eligendi et explicabo consequatur ea saepe tempore et voluptatum?
+            Quo voluptatum debitis eos repellat ipsa ut autem neque vel itaque
+            sunt atque omnis. Lorem ipsum dolor sit amet. Qui velit eligendi et
+            explicabo consequatur ea saepe tempore et voluptatum? Quo voluptatum
+            debitis eos repellat ipsa ut autem neque vel itaque sunt atque
+            omnis.
+          </p>
+        </div>
+        <video
+          autoPlay
+          loop
+          muted
+          className="relative z-10 w-auto min-w-full min-h-full max-w-none bg-blend-color-dodge bg-cover"
+          poster="/img_video3.png"
         >
-          <span className="text-green-400">About </span>
-          <span className="text-greyblue">Us</span>
-        </h1>
-        <p
-          data-aos="fade-up"
-          data-aos-duration="600"
-          className="mx-40 text-2xl bg-white bg-opacity-80 rounded-xl p-10 text-center border-solid border-2 border-greyblue"
-        >
-          Lorem ipsum dolor sit amet. Qui velit eligendi et explicabo
-          consequatur ea saepe tempore et voluptatum? Quo voluptatum debitis eos
-          repellat ipsa ut autem neque vel itaque sunt atque omnis. Lorem ipsum
-          dolor sit amet. Qui velit eligendi et explicabo consequatur ea saepe
-          tempore et voluptatum? Quo voluptatum debitis eos repellat ipsa ut
-          autem neque vel itaque sunt atque omnis. Lorem ipsum dolor sit amet.
-          Qui velit eligendi et explicabo consequatur ea saepe tempore et
-          voluptatum? Quo voluptatum debitis eos repellat ipsa ut autem neque
-          vel itaque sunt atque omnis. Lorem ipsum dolor sit amet. Qui velit
-          eligendi et explicabo consequatur ea saepe tempore et voluptatum? Quo
-          voluptatum debitis eos repellat ipsa ut autem neque vel itaque sunt
-          atque omnis. Lorem ipsum dolor sit amet. Qui velit eligendi et
-          explicabo consequatur ea saepe tempore et voluptatum? Quo voluptatum
-          debitis eos repellat ipsa ut autem neque vel itaque sunt atque omnis.
-          Lorem ipsum dolor sit amet. Qui velit eligendi et explicabo
-          consequatur ea saepe tempore et voluptatum? Quo voluptatum debitis eos
-          repellat ipsa ut autem neque vel itaque sunt atque omnis.
-        </p>
-      </div>
-      <div className="bg-[url('./assets/images/blurred-map.jpg')] contrast-75 bg-no-repeat bg-cover height-minus-nav w-screen blur-sm"></div>
+          <source
+            src="http://37.187.90.23/mapquest/vid/video3new.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </header>
     </div>
   );
 }
--- src/pages/Learn.jsx
+++ src/pages/Learn.jsx
@@ -31,16 +31,35 @@ const Learn = () => {
   }, [clickedCountry]);
 
   return (
-    <div className="height-minus-nav flex justify-center items-center bg-quiz">
-      <div className="h-4/5 w-[90%] p-1 relative flex rounded-lg border-solid border-2 border-slate-300 shadow-2xl bg-slate-300">
-        <SearchLearn setCountrySearchFound={setCountrySearchFound} />
-        <Map
-          actionOnClick={setClickedCountry}
-          clickedCountry={clickedCountry ? clickedCountry : countrySearchFound}
-          markerCoordinates={markerFoundCoordinate}
-        />
-        {clickedCountry && <Annotation country={clickedCountry} />}
+    <div>
+      <div className="absolute z-30 flex flex-col items-center height-minus-nav w-full">
+        <div className="height-minus-nav flex justify-center items-center w-full">
+          <div className="h-4/5 w-[90%] p-1 relative flex rounded-lg border-solid border-2 border-cyan-900 shadow-2xl bg-cyan-900">
+            <SearchLearn setCountrySearchFound={setCountrySearchFound} />
+            <Map
+              actionOnClick={setClickedCountry}
+              clickedCountry={
+                clickedCountry ? clickedCountry : countrySearchFound
+              }
+              markerCoordinates={markerFoundCoordinate}
+            />
+            {clickedCountry && <Annotation country={clickedCountry} />}
+          </div>
+        </div>
       </div>
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute z-10 w-auto min-w-full min-h-full max-w-none bg-blend-color-dodge object-scale-down"
+        poster="/img_video4.png"
+      >
+        <source
+          src="http://37.187.90.23/mapquest/vid/home4new.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
