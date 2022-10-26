353145b53ccd56e7f90904fb9b5793e65221b0a5
Gliem <maximilian.gliem@gmail.com>
2022-10-26 11:28:00 +0200

    readable timer

--- src/components/quizTimer/QuizTimer.jsx
+++ src/components/quizTimer/QuizTimer.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const QuizTimer = () => {
   const [timer, setTimer] = useState(0);
+  const [readableTimer, setReadableTimer] = useState("");
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -10,11 +11,36 @@ const QuizTimer = () => {
     return () => clearInterval(interval);
   }, []);
 
+  useEffect(() => {
+    let timeLeftAfterOperation = timer;
+    let hour = 0;
+    let minute = 0;
+    let seconde = 0;
+
+    if (timer >= 3600) {
+      hour = Math.floor(timer / 3600);
+      timeLeftAfterOperation = timeLeftAfterOperation % (3600 * hour);
+    }
+
+    if (timeLeftAfterOperation >= 60) {
+      minute = Math.floor(timeLeftAfterOperation / 60);
+      timeLeftAfterOperation = timeLeftAfterOperation % (60 * minute);
+    }
+
+    seconde = timeLeftAfterOperation;
+
+    setReadableTimer(
+      `${hour >= 10 ? hour : "0" + hour}:${
+        minute >= 10 ? minute : "0" + minute
+      }:${seconde >= 10 ? seconde : "0" + seconde}`
+    );
+  }, [timer]);
+
   return (
     <div className="z-50 absolute top-32 left-0 m-10">
       <div className="my-10 border-2 w-auto border-solid shadow-2xl p-4 rounded-2xl border-black bg-slate-100">
-        <h2 className="text-center w-full text-3xl mb-4">Timer</h2>
-        {timer}
+        <h2 className="text-center w-full text-3xl mb-4">Elapsed time:</h2>
+        <p className="w-fit m-auto text-xl">{readableTimer}</p>
       </div>
     </div>
   );
