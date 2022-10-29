bb0da6349fc0c491bda6840e5e1104860eec1976
Gliem <maximilian.gliem@gmail.com>
2022-10-29 22:36:19 +0200

    responsive on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,8 +10,23 @@ function Home() {
 
   return (
     <div className="flex justify-center">
-      <div className="absolute z-30 top-[80px] gap-24 flex flex-col justify-center items-center align height-minus-nav ">
-        <h1 className="text-8xl w-fit bg-slate-800 bg-opacity-0 rounded-md p-12 flex flex-col ">
+      <div className="absolute top-0 -z-10 h-[1100px] w-full md:h-screen min-h-[700px]">
+        <video
+          autoPlay
+          loop
+          muted
+          className="object-cover w-full h-full"
+          poster="./imgHome.png"
+        >
+          <source
+            src="http://37.187.90.23/mapquest/vid/home1reduce.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+      <div className="absolute z-30 top-[200px] gap-24 flex flex-col justify-center items-center align">
+        <h1 className="text-7xl md:text-8xl w-fit bg-slate-800 bg-opacity-0 rounded-md flex flex-col">
           <div>
             <span className="text-green-400">Map</span>
             <span className="text-lightblue">Quest</span>
@@ -21,14 +36,14 @@ function Home() {
           </span>
         </h1>
 
-        <div className="flex gap-20">
+        <div className="flex flex-col md:flex-row gap-20">
           <Link to="/learn">
             <div
               className="card w-80 hover:shadow-2xl"
               data-aos="fade-in"
               data-aos-duration="500"
             >
-              <div className=" bg-norepeat rounded-t-md bg-cover bg-[url('./img_video4.png')] h-44"></div>
+              <div className=" bg-norepeat rounded-t-md bg-cover bg-[url('/img_video4.png')] h-44"></div>
               <p className="text-xl bg-slate-800 text-white bg-opacity-80 p-4 text-center rounded-b-md ">
                 Explore new countries
               </p>
@@ -39,29 +54,15 @@ function Home() {
               className="card w-80 hover:shadow-2xl"
               data-aos="fade-in"
               data-aos-duration="1200"
+              data-aos-offset="-10"
             >
-              <div className=" bg-norepeat rounded-t-md bg-cover bg-[url('./img_video3.png')] h-44"></div>
+              <div className=" bg-norepeat rounded-t-md bg-cover bg-[url('/img_video3.png')] h-44"></div>
               <p className="text-xl bg-slate-800 text-white bg-opacity-80 p-4 text-center rounded-b-md ">
                 Test your knowledge
               </p>
             </div>
           </Link>
         </div>
-      </div>
-      <div className="z-10 h-full absolute w-full">
-        <video
-          autoPlay
-          loop
-          muted
-          poster="/imgHome.png"
-          className="absolute object-fill w-full h-full"
-        >
-          <source
-            src="http://37.187.90.23/mapquest/vid/home1reduce.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
       </div>
     </div>
   );
