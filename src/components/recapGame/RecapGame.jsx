e79c040eb00264f4a293c4d5e9a9e481905026d3
Gliem <maximilian.gliem@gmail.com>
2022-10-31 12:08:59 +0100

    show previous timer and score

--- src/components/recapGame/RecapGame.jsx
+++ src/components/recapGame/RecapGame.jsx
@@ -1,15 +1,21 @@
+import readableTimer from "../../helpers/readableTimer";
+
 const RecapGame = ({
-  counterCorrect,
+  score,
   counterQuestion,
   questionNumber,
   response,
+  timeToAnswer,
 }) => {
   return (
     <div className="z-50 absolute top-32 right-0 m-10">
       <div className="my-10 w-auto shadow-2xl p-4 rounded-md border-black bg-slate-800 text-white">
         <h2 className="text-center w-full text-3xl mb-4">Details</h2>
         <p>{`Question : ${counterQuestion}/${questionNumber}`}</p>
-        <p>Score : {counterCorrect}</p>
+        {timeToAnswer && (
+          <p>{`You responded in ${readableTimer(timeToAnswer)}`}</p>
+        )}
+        <p>Score : {score}</p>
         <p>
           {response !== "" && "Previous correct answer :"} {response}
         </p>
