c4b06d24fc916cb180f906b14174c44ac27b24a8
Ysedda <arthur.baudoin@icloud.com>
2022-11-03 16:35:34 +0100

    overflow adjusted and recap reordered

--- src/components/recapGame/RecapGame.jsx
+++ src/components/recapGame/RecapGame.jsx
@@ -13,13 +13,13 @@ const RecapGame = ({
     <div className="z-50 absolute top-32 right-0 m-10">
       <div className="my-10 w-auto shadow-2xl p-4 rounded-md border-black bg-slate-800 text-white">
         <h2 className="text-center w-full text-3xl mb-4">Details</h2>
-        <p>{`Question : ${counterQuestion}/${questionNumber}`}</p>
-        {timeToAnswer && (
-          <p>{`You responded in ${readableTimer(timeToAnswer)}`}</p>
-        )}
         <p>Score : {score}</p>
-        <p>{responseDone !== "" && `You answered ${responseDone}.`}</p>
-
+        <p>{`Question : ${counterQuestion}/${questionNumber}`}</p>
+        {timeToAnswer && responseDone !== "" && (
+          <p>{`You answered ${responseDone} in ${readableTimer(
+            timeToAnswer
+          )}`}</p>
+        )}
         {response !== "" &&
           (numberResponseDone === 0 ? (
             <p>{`${response} was the answer!`} </p>
